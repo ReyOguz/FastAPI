c59e2dcdd4d4e4843129753a40a7100b2c99df94
ReyOguz <reyoguz00@gmail.com>
2025-02-11 18:16:09 +1100

    added CORS middleware to main to potentially allow other domains to send reqeusts to our api and also added the requirements.txt file to allow others that clone our repo to download all of our dependancies and run our backend

--- app/main.py
+++ app/main.py
@@ -4,9 +4,27 @@ from .routers import posts, users, auth, votes
 from . import models
 from . database import engine
 
-# models.Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# The origins list is a list of domains that can communicate with this backend. If a domain is not listed here, it will be blocked by CORS
+# SECURITY BEST PRACTICE IS TO NARROW THIS LIST DOWN AS MUCH AS POSSIBLE AND NOT ALLOW THE PROCESSING OF UNWANTED REQUESTS
+origins = []
+
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  # allow_methods is also another attribute input arg where we can define what type of HTTP requests will will allow to be sent to our backend
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+@app.get("/")
+def main():
+  return {"msg": "main page"}
 
 # Including router for posts endpoints
 app.include_router(posts.router)
